3f9af9323ddccf1cd96ebd0a2cd1e42e229327bf
Rafiul29 <rafiulislam4472@gmail.com>
2023-08-10 23:55:12 +0600

    find a single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,31 +1,54 @@
 // external import
-const Product=require('../model/productModel')
+const Product = require("../model/productModel");
+const mongoose=require("mongoose")
 
 // add a new product
-const addProduct=async(req,res)=>{
-    try{
-        const product=await Product.create({...req.body});
-        res.status(200).json(product)
-    }catch(error){
-      res.status(500).json({
-        error:error.message
-      })
-    }
-}
+const addProduct = async (req, res) => {
+  try {
+    const product = await Product.create({ ...req.body });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 // get all products
-const getAllProducts=async(req,res)=>{
-  try{
-    const products=await Product.find({}).sort({ createdAt: -1 });
-    res.status(200).json(products)
-  }catch(error){
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
     res.status(500).json({
-      error:error.message
-    })
+      error: error.message,
+    });
   }
-}
+};
 
-module.exports={
+// get a single product
+const getSingleProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "invalid id" });
+    }
+
+    const singleProduct = await Product.findById(id);
+
+    if (!singleProduct) {
+      return res.status(404).json({ error: "No product found" });
+    }
+    
+    res.status(200).json(singleProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
   addProduct,
-  getAllProducts
-}
+  getAllProducts,
+  getSingleProduct,
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,15 +2,20 @@
 const express = require('express');
 
 // internal import 
-const {addProduct, getAllProducts}=require("../controllers/productController")
+const {addProduct, getAllProducts,getSingleProduct}=require("../controllers/productController")
 
 // router
 const route=express.Router();
+
 
 // add new product
 route.post("/",addProduct)
 
 // get all products
 route.get("/",getAllProducts)
+
+// get sigle product
+route.get("/:id",getSingleProduct)
+
 
 module.exports=route;
